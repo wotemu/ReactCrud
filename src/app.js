681bf81a3dbe2b90a2f5a7d77cc4e6f012a3b47f
Workneh Tamire <workneh.tamire@aalto.fi>
2018-05-25 19:25:56 +0300

    Removed dummy data

--- src/app.js
+++ src/app.js
@@ -11,16 +11,16 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'water bill', amount:4500}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt:1000}));
-store.dispatch(addExpense({description: 'Rent', amount:109500}));
-store.dispatch(setTextFilter(''));
+// store.dispatch(addExpense({description: 'water bill', amount:4500}));
+// store.dispatch(addExpense({description: 'Gas bill', createdAt:1000}));
+// store.dispatch(addExpense({description: 'Rent', amount:109500}));
+// store.dispatch(setTextFilter(''));
 
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-console.log(visibleExpenses);
+// console.log(visibleExpenses);
 const jsx = (
   <Provider  store = {store}>
     <AppRouter />
